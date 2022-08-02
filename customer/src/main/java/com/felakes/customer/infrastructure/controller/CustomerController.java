45c9e0e4b0673bf7fd1d992dd88f4ce0e270f39e
Felipe Lagos <felagossanchez@gmail.com>
2022-08-02 14:04:39 -0400

    fix controller

--- customer/src/main/java/com/felakes/customer/infrastructure/controller/CustomerController.java
+++ customer/src/main/java/com/felakes/customer/infrastructure/controller/CustomerController.java
@@ -1,5 +1,13 @@
-package io.github.jiangdequan;
+package com.felakes.customer.infrastructure.controller;
 
+import static com.felakes.customer.infrastructure.utils.Constants.API_CUSTOMER;
+import static com.felakes.customer.infrastructure.utils.Constants.API_VERSION;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = API_VERSION + API_CUSTOMER)
 public class CustomerController {
-    
+
 }
---
+++ customer/src/main/java/com/felakes/customer/infrastructure/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.felakes.customer.infrastructure.utils;
+
+public class Constants {
+
+    public static final String API_VERSION = "/api/v1";
+    public static final String API_CUSTOMER = "/customers";
+
+}

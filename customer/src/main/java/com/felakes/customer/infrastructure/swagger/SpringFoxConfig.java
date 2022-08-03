87477f2c813cad327e42e8b14e5c795d5b67aa97
Felipe Lagos <flagos@bancointernacional.cl>
2022-08-03 10:14:32 -0400

    config swagger

--- customer/src/main/java/com/felakes/customer/infrastructure/swagger/SpringFoxConfig.java
+++ customer/src/main/java/com/felakes/customer/infrastructure/swagger/SpringFoxConfig.java
@@ -8,10 +8,10 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-//@EnableSwagger2
+@EnableSwagger2
 public class SpringFoxConfig {
 
-    //@Bean
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()

f62e38dc74a470dc14aa143127fc2f1c96aefe6e
Felipe Lagos <felagossanchez@gmail.com>
2022-08-04 23:17:40 -0400

    migrate to springdoc

--- product/src/main/java/com/felakes/product/ProductApplication.java
+++ product/src/main/java/com/felakes/product/ProductApplication.java
@@ -2,12 +2,14 @@ package com.felakes.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class Productpplication {
+@EnableDiscoveryClient
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Productpplication.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
